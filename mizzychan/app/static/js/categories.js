35859f83fd5e5e5ca9ea2deb9862a99909bf76be
Dark <cczechowicz@gmail.com>
2016-10-23 03:02:08 -0400

    clock working

--- mizzychan/app/static/js/categories.js
+++ mizzychan/app/static/js/categories.js
@@ -1,7 +1,30 @@
 console.log('gsg')
 
-var clock = $('.your-clock').FlipClock(900, {
+var now = new Date();
+var minutes = now.getMinutes();
+var seconds = now.getSeconds();
+console.log (minutes)
+
+var clock = $('.your-clock').FlipClock({
 	countdown: true,
-	clockFace: 'MinuteCounter'
-	
-});
+    stop: function() {
+	    // do something
+	    // reset the counter and it will start again
+	    this.setTime(900);
+    }
+  });
+
+minutes_seconds = minutes * 60 +seconds; 
+console.log (minutes_seconds)
+
+timestuff = minutes_seconds;
+do{
+    timestuff = timestuff - 900;
+}
+while (timestuff >= 900);
+
+timestuff = (timestuff - 900)*(-1);
+
+clock.setTime(timestuff);
+clock.start();
+console.log (timestuff);
